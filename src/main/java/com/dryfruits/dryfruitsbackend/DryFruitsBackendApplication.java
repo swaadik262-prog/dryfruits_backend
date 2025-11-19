76a29a7ea7b958d4c52720e0c1b909fe50f541f9
HimanshuSoni5122001 <himanshusoni9587@gmail.com>
2025-11-19 14:31:04 +0530

    update main class name

--- src/main/java/com/dryfruits/dryfruitsbackend/DryFruitsBackendApplication.java
+++ src/main/java/com/dryfruits/dryfruitsbackend/DryFruitsBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DryfruitsbackendApplication {
+public class DryFruitsBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DryfruitsbackendApplication.class, args);
+		SpringApplication.run(DryFruitsBackendApplication.class, args);
 		System.out.println("Hello Java");
 	}
 }
