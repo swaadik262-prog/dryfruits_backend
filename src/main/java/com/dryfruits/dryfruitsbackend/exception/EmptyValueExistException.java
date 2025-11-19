2d0243326b368ae4a9d96758b1e3895c7b4909df
HimanshuSoni5122001 <himanshusoni9587@gmail.com>
2025-11-19 14:24:38 +0530

    update code

--- src/main/java/com/dryfruits/dryfruitsbackend/exception/EmptyValueExistException.java
+++
@@ -1,4 +0,0 @@
-package com.dryfruits.dryfruitsbackend.exception;
-
-public class EmptyValueExistException extends Throwable {
-}
--- src/main/java/com/dryfruits/dryfruitsbackend/util/Util.java
+++
@@ -1,6 +0,0 @@
-package com.dryfruits.dryfruitsbackend.util;
-
-public class Util {
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String BEARER = "Bearer ";
-}
